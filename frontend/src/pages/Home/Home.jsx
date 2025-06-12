6447b0b835e9b0ec3ab073ad7e4828c24a47fc85
genevieve <cherigen@neuf.fr>
2025-06-12 14:44:50 +0200

    Mise en place du composant 'carrousel' en prévision de la dynamisation, des titres et boutons de liaison des pages livres détaillé et inscription

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,6 +13,17 @@ export default function Home() {
             <h1>Votre prochaine lecture vous attend</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam eveniet ea! Harum dolores aspernatur sit adipisci officiis, tempore cupiditate debitis consequatur, est quae neque, quod error veniam et consequuntur.
             </p>
+            <h2>Notre sélection</h2>
+
+            {/*Carrousel : composant BookPreview qui sera dynamisé ultérieurement}
+            <BookPreview/>
+            */}
+
+            <div>
+            {/*Ancre dans href en attendant d'avoir le chemin exact de la page /library/bookID lorsqu'elle sera créée et class btn en attendant d'avoir le SCSS relatif*/}
+            <a classe="btn" href="#">voir le détail</a>
+              </div> 
+
             {/* Finir de modifier ici */}
         </>
     )
