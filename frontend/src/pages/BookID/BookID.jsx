f6df902b56d0591fafa50db986d3b795c63d09ed
GCZ58 <cherge91@gmail.com>
2025-06-16 16:11:29 +0200

    Réinitialisation de contact après suppression de la branche

---
+++ frontend/src/pages/BookID/BookID.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router';
+// Import du [Composant].scss
+import './BookID.scss';
+
+
+export default function BookID() {
+    return(
+        <>
+            <h1>Nous contacter</h1>
+
+           
+        </>
+    )
+}
