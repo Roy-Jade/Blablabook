ba0f37b2c4cbbbef11d76182236ac9c794f2ba15
Grégoire Portier <gregoire.portier@oclock.school>
2025-07-04 16:12:01 +0200

    Ajout favicon et affichage des options des bookmini conditionnés par la connexion

--- frontend/src/components/BookMinis/BookMini/BookMini.jsx
+++ frontend/src/components/BookMinis/BookMini/BookMini.jsx
@@ -1,5 +1,7 @@
 // On importe notre [Composant].scss
 import './BookMini.scss';
+import { CurrentUserContext } from '../../../Contexts';
+import { useContext } from 'react';
 
 // On créée une fonction qui contient un return 
 // Le return doit comprendre une balise englobant tout le reste. Utiliser une balise vide <> fonctionne.
@@ -7,6 +9,9 @@ import './BookMini.scss';
 
 // L'écriture ci-dessus permet de faire la fonction et de l'exporter en même temps
 export default function BookMini({book, newClass}) {
+
+  const currentUser = useContext(CurrentUserContext);
+
   return(
     <article className={`bookmini ${newClass}`}>
       <img className='bookmini__img' src={"https://covers.openlibrary.org/b/isbn/"+book.isbn+"-M.jpg"} alt="Couverture" />
@@ -17,17 +22,20 @@ export default function BookMini({book, newClass}) {
         </div>
         <div className='bookmini__note'>&#9733; &#9733; &#9733; &#9733; &#9734;</div>
         <button className='button button_small'>Voir le détail</button>
-        <div className='bookmini__booleans connected owned'>
-          <div>
-            <input type="checkbox" id='isRead' name='isRead'/>
-            <label htmlFor="isRead">Lu</label>
+        
+        {currentUser.currentUser && (<>
+          <div className='bookmini__booleans connected owned'>
+            <div>
+              <input type="checkbox" id='isRead' name='isRead'/>
+              <label htmlFor="isRead">Lu</label>
+            </div>
+            <div>
+              <input type="checkbox" id='isShared' name='isShared'/>
+              <label htmlFor="isShared">Partagé</label>
+            </div>
           </div>
-          <div>
-            <input type="checkbox" id='isShared' name='isShared'/>
-            <label htmlFor="isShared">Partagé</label>
-          </div>
-        </div>
-        <button className='button button_small connected not_owned'>Ajouter</button>
+          <button className='button button_small connected not_owned'>Ajouter</button>
+        </>)}
       </div>
     </article>
   )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,9 @@
 import {Link} from 'react-router';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import BookMinis from '../../components/BookMinis/BookMinis';
 import Carousel from '../../components/Carousel/Carousel';
 import './Home.scss';
 
-// On créée une fonction qui contient un return 
-// Le return doit comprendre une balise englobant tout le reste. Utiliser une balise vide <> fonctionne.
-// On, doit enfin exporter la fonction en default sous le nom de notre composant
-
-// L'écriture ci-dessus permet de faire la fonction et de l'exporter en même temps
 export default function Home() {
 
     return(
@@ -18,7 +12,6 @@ export default function Home() {
                 <title>Accueil - BlablaBook</title>
                 <meta name='description' content='Trouvez votre livre préféré'></meta>
             </Helmet>
-            {/* Commencer à modifier ici */}
             <h1>Votre prochaine lecture vous attend</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eveniet labore exercitationem ab aliquid dicta laborum autem maxime aperiam, accusamus quos ducimus, minus consequuntur! Temporibus in perspiciatis recusandae. Laborum, veritatis.
             Sunt, possimus. </p>
@@ -27,8 +20,6 @@ export default function Home() {
             <Carousel />
 
             <Link className='button button_big' to="/register">S'inscrire</Link>
-
-            {/* Finir de modifier ici */}
         </>
     )
 }
