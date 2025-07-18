124f068aae884c8d3edacc8b62323b671ef59090
laurent <laurent.neveux@oclock.io>
2025-07-18 20:51:39 +0200

    fix db connexion

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import 'dotenv/config';
 import cors from 'cors';
 import router from './routers/router.js';
 
-dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
